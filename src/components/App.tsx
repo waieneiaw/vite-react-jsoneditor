f70c98e06dd73504d9657a015d15306cd4a6ea96
waieneiaw <15177530+waieneiaw@users.noreply.github.com>
2022-12-26 08:09:11 +0000

    mod: move app component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from '../assets/react.svg'
 import './App.css'
 
-function App() {
+export const App = () => {
   const [count, setCount] = useState(0)
 
   return (
@@ -30,5 +30,3 @@ function App() {
     </div>
   )
 }
-
-export default App
